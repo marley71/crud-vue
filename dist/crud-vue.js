72bb1cef95e0b8f85fd316d642f1277050fd2b6d
Pier Paolo Ciullo <pierpaolo.ciullo@buongiorno.com>
2019-12-27 17:07:48 +0100

    bugfixing

--- dist/crud-vue.js
+++ dist/crud-vue.js
@@ -3522,9 +3522,9 @@ Vue.component('r-select',{
 Vue.component('r-radio',{
     extends : Crud.components.renders.rBase,
     data :  function () {
-        //console.log('c-select',this.cData);
-        var dV = this.cConf.metadata.domainValues;
-        var dVO = this.cConf.metadata.domainValuesOrder?this.cConf.metadata.domainValuesOrder:Object.keys(dV);
+        var metadata = this.cConf.metadata || {};
+        var dV = metadata.domainValues || {};
+        var dVO = metadata.domainValuesOrder?metadata.domainValuesOrder:Object.keys(dV);
         return {
             name : this.cConf.name,
             value: this.cConf.value,
--- src/js/renders/r-radio.js
+++ src/js/renders/r-radio.js
@@ -1,9 +1,9 @@
 Vue.component('r-radio',{
     extends : Crud.components.renders.rBase,
     data :  function () {
-        //console.log('c-select',this.cData);
-        var dV = this.cConf.metadata.domainValues;
-        var dVO = this.cConf.metadata.domainValuesOrder?this.cConf.metadata.domainValuesOrder:Object.keys(dV);
+        var metadata = this.cConf.metadata || {};
+        var dV = metadata.domainValues || {};
+        var dVO = metadata.domainValuesOrder?metadata.domainValuesOrder:Object.keys(dV);
         return {
             name : this.cConf.name,
             value: this.cConf.value,
