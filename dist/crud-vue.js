6226fefb080a1c0ad481b4f4703d1fbc097a2bb7
Pier Paolo Ciullo <pierpaolo.ciullo@buongiorno.com>
2020-06-30 16:29:07 +0200

    bugfix metodo isHidden

--- dist/crud-vue.js
+++ dist/crud-vue.js
@@ -3306,9 +3306,9 @@ crud.components.views.vBase = Vue.component('v-base', {
                 if (typeof this.fieldsConfig[key] === 'string' || this.fieldsConfig[key] instanceof String)
                     type = this.fieldsConfig[key];
                 else
-                    type = 'w-hidden';
+                    type = this.fieldsConfig[key].type?this.fieldsConfig[key].type:type;
 
-                if (type == 'w-hidden')
+                if (type === 'w-hidden')
                     return true;
             }
             return false;
