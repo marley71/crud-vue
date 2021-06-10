51799770847aae636c1bbaad13a871c42a357d0b
vagrant <vagrant@homestead>
2021-06-10 08:01:31 +0000

    bugfix getValue hidden

--- crud-sfc/core/mixins/components/widgets/wHiddenMixin.js
+++ crud-sfc/core/mixins/components/widgets/wHiddenMixin.js
@@ -4,6 +4,14 @@ crud.conf['w-hidden'] = {
 }
 
 const wHiddenMixin = {
-
+    methods : {
+        getValue() {
+            return this.jQe('input').val();
+        },
+        setValue(value) {
+            this.value = value;
+            this.jQe('input').val(value);
+        }
+    }
 }
 export default wHiddenMixin
--- crud-sfc/core/mixins/components/widgets/wInputMixin.js
+++ crud-sfc/core/mixins/components/widgets/wInputMixin.js
@@ -6,6 +6,14 @@ crud.conf['w-input'] = {
 }
 
 const wInputMixin = {
-
+    methods : {
+        getValue() {
+            return this.jQe('input').val();
+        },
+        setValue(value) {
+            this.value = value;
+            this.jQe('input').val(value);
+        }
+    }
 }
 export default wInputMixin
