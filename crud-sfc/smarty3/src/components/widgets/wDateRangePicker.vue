c06b261ee35bff4fec44629c29219b8a9298255b
vagrant <vagrant@homestead>
2021-09-01 09:35:50 +0000

    date-range-picker fixato

--- crud-sfc/smarty3/src/components/widgets/wDateRangePicker.vue
+++ crud-sfc/smarty3/src/components/widgets/wDateRangePicker.vue
@@ -5,8 +5,8 @@
 <!--        <input class="px-3 py-3 placeholder-gray-400 text-gray-700 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full" c-picker>-->
 <!--    </div>-->
   <div class="input-group">
-    <input type="hidden" v-model="value" v-bind:name="getFieldName()"
-           v-on:change="change">
+    <input type="hidden" :name="startFieldName" v-on:change="change">
+    <input type="hidden" :name="endFieldName" v-on:change="change">
     <input class="form-control" c-picker>
   </div>
 </template>
@@ -16,16 +16,20 @@
 import crud from '../../../../core/crud'
 import wBase from './wBase'
 
+/* global moment */
+/* eslint no-undef: "error" */
+
 crud.conf['w-date-range-picker'] = {
   resources: [
     'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js',
     'https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js',
     'https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css'
   ],
-  displayFormat: 'dd/mm/yyyy',
-  dateFormat: 'yyyy-mm-dd',
+  displayFormat: 'DD/MM/YYYY',
+  dateFormat: 'YYYY-MM-DD',
   startFieldName: null,
-  endFieldName: null
+  endFieldName: null,
+  pluginOptions: {}
 }
 
 export default {
@@ -33,10 +37,25 @@ export default {
   extends: wBase,
   methods: {
     _ready () {
-      this.jQe('[c-picker]').daterangepicker({
-        opens: 'left'
-      }, function (start, end, label) {
+      var that = this
+      if (!that.startFieldName) {
+        throw new Error('wRangeDatePicker startFieldName non definito')
+      }
+      that.jQe('input[name="' + that.startFieldName + '"]').val(that.modelData[that.startFieldName])
+      that.jQe('input[name="' + that.endFieldName + '"]').val(that.modelData[that.endFieldName])
+      var _opt = {
+        opens: 'left',
+        startDate: moment(that.modelData[that.startFieldName]),
+        endDate: moment(that.modelData[that.endFieldName]),
+        locale: {
+          format: that.displayFormat
+        }
+      }
+      var options = that.merge(_opt, that.pluginOptions)
+      this.jQe('[c-picker]').daterangepicker(options, function (start, end, label) {
         console.log('A new date selection was made: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'))
+        that.jQe('input[name="' + that.startFieldName + '"]').val(start.format(that.dateFormat))
+        that.jQe('input[name="' + that.endFieldName + '"]').val(end.format(that.dateFormat))
       })
     }
   }
