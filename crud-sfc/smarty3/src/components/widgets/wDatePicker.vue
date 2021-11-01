e1366b4ab796a4d5cda7239888361cfd68d94357
vagrant <vagrant@homestead>
2021-11-01 18:38:51 +0000

    migliorato pulsante data-picker

--- crud-sfc/smarty3/src/components/widgets/wDatePicker.vue
+++ crud-sfc/smarty3/src/components/widgets/wDatePicker.vue
@@ -22,14 +22,22 @@
 <!--    </div>-->
 <!--  </div>-->
 
-  <div class="input-group mb-3">
-    <input class="form-control datepicker" c-picker :aria-label="label"  aria-describedby="basic-addon1">
-    <div v-if="buttonClear" class="input-group-append">
-      <span v-on:click="reset()" class="input-group-text -mouse-pointer" id="basic-addon1">
-        <i class="fa fa-times"></i>
-        <input type="hidden" v-model="value" v-bind:name="getFieldName()" v-on:change="change">
-      </span>
-    </div>
+<!--  <div class="input-group mb-3">-->
+<!--    <div class="input-group form-control">-->
+<!--      <input class="w-100 border-0 datepicker" c-picker>-->
+<!--    </div>-->
+<!--    <div v-if="buttonClear" class="input-group-append">-->
+<!--      <span v-on:click="reset()" class="input-group-text -mouse-pointer" id="basic-addon1">-->
+<!--        <i class="fa fa-times"></i>-->
+<!--        <input type="hidden" v-model="value" v-bind:name="getFieldName()" v-on:change="change">-->
+<!--      </span>-->
+<!--    </div>-->
+<!--  </div>-->
+
+  <div class="input-group form-control">
+    <input class="w-100 border-0 datepicker" c-picker >
+    <input type="hidden" v-model="value" v-bind:name="getFieldName()" v-on:change="change">
+    <button v-if="buttonClear" class="close-icon" type="button" v-on:click="reset()"></button>
   </div>
 </template>
 
@@ -50,5 +58,34 @@ export default {
 </script>
 
 <style scoped>
+.close-icon {
+  border:1px solid transparent;
+  background-color: transparent;
+  display: inline-block;
+  vertical-align: middle;
+  outline: 0;
+  cursor: pointer;
+}
 
+.close-icon:after {
+  content: "X";
+  display: block;
+  width: 15px;
+  height: 15px;
+  position: absolute;
+  background-color: #AAAAAA;
+  z-index:1;
+  right: 35px;
+  top: 0;
+  bottom: 1px;
+  margin: auto;
+  padding: 2px;
+  border-radius: 50%;
+  text-align: center;
+  color: white;
+  font-weight: normal;
+  font-size: 12px;
+  box-shadow: 0 0 2px #000000;
+  cursor: pointer;
+}
 </style>
