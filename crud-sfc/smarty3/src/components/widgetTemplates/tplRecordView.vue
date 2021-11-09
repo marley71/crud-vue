a38f4e5f3382af3ea3b8e3b4bbc019237b2c91f7
vagrant <vagrant@homestead>
2021-11-09 09:35:41 +0000

    migliorata label tpl-record-view

--- crud-sfc/smarty3/src/components/widgetTemplates/tplRecordView.vue
+++ crud-sfc/smarty3/src/components/widgetTemplates/tplRecordView.vue
@@ -1,16 +1,16 @@
 <template>
     <component v-if="cWidget.type=='w-hidden'" :is="cWidget.type" :c-conf="cWidget"></component>
     <div :class="getLayoutClass()" v-else>
-        <div v-if="cTemplate.labelType === 'top'">
+        <div class="text-gray-500 label" v-if="cTemplate.labelType === 'top'">
             {{cWidget.label}}
         </div>
         <div>
-            <span v-if="cTemplate.labelType === 'inline'">
+            <span class="text-gray-500 label" v-if="cTemplate.labelType === 'inline'">
               {{cWidget.label}}
             </span>
             <component :is="cWidget.type" :c-conf="cWidget"></component>
         </div>
-        <div v-if="cTemplate.labelType === 'bottom'">
+        <div class="text-gray-500 label" v-if="cTemplate.labelType === 'bottom'">
           {{cWidget.label}}
         </div>
     </div>
@@ -21,10 +21,21 @@ import tplBase from './tplBase'
 
 export default {
   name: 'tpl-record-view',
-  extends: tplBase
+  extends: tplBase,
+  data () {
+    if (!this.cTemplate.layoutType) {
+      this.cTemplate.layoutType = 'half'
+    }
+    if (!this.cTemplate.labelType) {
+      this.cTemplate.labelType = 'top'
+    }
+    return {}
+  }
 }
 </script>
 
 <style scoped>
-
+  .label {
+    font-size: .8em;
+  }
 </style>
